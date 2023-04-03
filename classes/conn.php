1a39fbadec6a7ff83cfcddd562d7418c53081a1a
Mehdi Joumad <100050754+eelx001@users.noreply.github.com>
2023-04-03 00:47:21 +0000

    Update conn.php

--- classes/conn.php
+++ classes/conn.php
@@ -1,9 +1,9 @@
 <?php 
 
 
-$dsn = 'mysql:host=localhost;dbname=mip';
-$user = 'root';
-$pass = '';
+$dsn = 'mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_34dbae8beb91c36';
+$user = 'b1d30d637b6822';
+$pass = 'd84e8674';
 $options = array(
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 );
